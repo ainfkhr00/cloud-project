faebb27ce6494035fd2a829b14a9499e10330cca
Ain Fakhri <81550780+ainfkhr00@users.noreply.github.com>
2022-06-22 22:23:57 +0800

    Add files via upload

---
+++ Todo-List-App-main/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { TodoMain } from './components/TodoMain.js';
+import AppCSS from './App.css';
+
+function App() {
+  return <section className='py-14 bg-[#222222]'>
+    <TodoMain />
+  </section>
+}
+
+export default App;
